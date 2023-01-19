9dba68538fa1c5a49f3b33e788b58e3b70269e58
Appaso Borate <appaso1686@gmail.com>
2023-01-19 17:54:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# OpenAI_Buttonclick.github.io" 
---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1637439.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 117944446, 117944447, 117944896, 117944897, 117944899, 117944902, 118321248, 118481720 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/117944465/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/117944461/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/117944460/1/ammo.js', 'preload' : true},
+];
